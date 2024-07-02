2f9fef5a8968364e94a7e6004df1e13e29fd1c38
MAACJR032 <maacamposjr@gmail.com>
2024-07-02 20:06:05 -0300

    Starting to implement the play function
    Creating logic to check which grid the player will choose to play and Creating play_sub to mark the selected sub grid

--- TicTacToe.h
+++ TicTacToe.h
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <vector>
+#include <unistd.h>
 
 using std::vector;
 using std::cout;
@@ -29,6 +30,7 @@ class TicTacToe
     private:
         vector<vector<status>> tic_tac_toe;
         vector<status> grids;
+        void play_sub(enum status player, int square, int sub_square);
 
     public:
         TicTacToe();
@@ -44,6 +46,20 @@ TicTacToe::TicTacToe()
         tic_tac_toe[i].resize(9, EMPTY);
     
     grids.resize(9, EMPTY);
+}
+
+void TicTacToe::play_sub(status player, int square, int sub_square)
+{
+    switch (sub_square)
+    {
+        case 1:
+            tic_tac_toe[0][0] = player;
+            
+            break;
+        
+        default:
+            break;
+    }
 }
 
 void TicTacToe::print_tic_tac_toe()
@@ -64,26 +80,49 @@ void TicTacToe::print_tic_tac_toe()
     }
 }
 
-void TicTacToe::play(enum status player, int square, int sub_square)
+void TicTacToe::play(status player, int square, int sub_square)
 {
-    /* Criar função para checar se o quadrado já foi completado, para não jogar e local já dominado */
-    /* Criar função para checar se os quadrados selecionados são válidos */
-    do
-    {
-        cout << "Square: ";
-        cin >> square;
+    // if the square to play is occupied, than the player can play in any free grid
+    // while (grids[square] != EMPTY)
+    // {
+    //     print_tic_tac_toe();
+    //     cin >> square;
+    // }
 
-        cout << "Sub square: ";
-        cin >> sub_square;
-
-    } while (square < 0 || square > 9 || sub_square < 0 || sub_square > 9);
+    cout << "Grid: ";
+    cin >> square;
     
+    while (square < 0 || square > 8 || grids[square] != EMPTY) 
+    {
+        cout << "Invalid grid\n";
+        cout << "You can play in these grids: ";
+
+        for (size_t i = 0; i < 9; i++)
+            if (grids[i] == EMPTY)
+                cout << i << " ";
+        cout << '\n';
+    }
+    
+    cout << "Sub grid: ";
+    cin >> sub_square;
+
+    sleep(2);
+
     switch (square)
     {
+        /* Limit of tic_tac_toe: 
+            i: 0 - 2 and j: 0 - 2*/ 
         case 1:
-            
+            play_sub(player, square, sub_square);
+            break;
+        
+        default:
             break;
     }
+    
+    /* Criar função para checar se o quadrado já foi completado, para não jogar em local já dominado */
+    /* Criar função para checar se os quadrados selecionados são válidos */
+   
 }
 
 enum status TicTacToe::win(enum status player)
