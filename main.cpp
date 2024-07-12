d5543b14ee22cdab9df073da0a9f4b8b63c30c65
MAACJR032 <maacamposjr@gmail.com>
2024-07-11 21:40:08 -0300

    Start of SFML implementation

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
-#include "TicTacToe.hpp"
+#include <SFML/graphics.hpp>
 #include <fstream>
+#include "include/TicTacToe.hpp"
 
 using std::ifstream;
 
@@ -29,74 +30,108 @@ void clear_buffer()
 
 int main()
 {
-    TicTacToe t;
-    string player1, player2;
+    sf::RenderWindow window(sf::VideoMode(900, 900), "Super Tic Tac Toe", sf::Style::Close);
+    sf::CircleShape shape(250.f);
+    shape.setFillColor(sf::Color::Green);
 
-    cout << "Player 1 (X) name: ";
-    cin >> player1;
-    cout << "Player 2 (O) name: ";
-    cin >> player2;
-
-    /* Instructions */
-    clear_buffer();
-    cout << "\nDo you wanna read the instructions ? [Y/N]: ";
-
-    if (tolower(getchar()) == 'y')
+    while (window.isOpen())
     {
-        ifstream file;
-        file.open("instructions.txt");
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            switch (event.type)
+            {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                
+                // pressing Esc to close
+                case sf::Event::KeyPressed:
+                    if (event.key.code == sf::Keyboard::Escape)
+                        window.close();
+                    break;
 
-        if (file.fail())
-            cout << "Error opening file\n";
+                default:
+                    break;
+            }   
+        }
 
-        cout << file.rdbuf();
-        file.close();
+        // Render
+        window.clear(sf::Color::White); // clear old frame
+
+        window.draw(shape);
         
-        clear_buffer();
-        cout << "\n\nPress Enter to close\n";
+        window.display(); // done drawing
     }
-    clear_buffer();
 
-    /* Game Loop */ 
-    status turn = X;
-    int round = 1;
+    // TicTacToe t;
+    // string player1, player2;
 
-    while (victory == EMPTY)
-    {
-        cout << "Round " << round << '\n';
-        sleep(2);
+    // cout << "Player 1 (X) name: ";
+    // cin >> player1;
+    // cout << "Player 2 (O) name: ";
+    // cin >> player2;
 
-        if (turn == X)
-        {
-            play(t, X, player1);
-            turn = O; // Change turn
-        }
-        else
-        {
-            play(t, O, player2);
-            turn = X; // Change turn
-        }
+    // /* Instructions */
+    // clear_buffer();
+    // cout << "\nDo you wanna read the instructions ? [Y/N]: ";
 
-        round++;
-        clear();
-    }
+    // if (tolower(getchar()) == 'y')
+    // {
+    //     ifstream file;
+    //     file.open("instructions.txt");
+
+    //     if (file.fail())
+    //         cout << "Error opening file\n";
+
+    //     cout << file.rdbuf();
+    //     file.close();
+        
+    //     clear_buffer();
+    //     cout << "\n\nPress Enter to close\n";
+    // }
+    // clear_buffer();
+
+    // /* Game Loop */ 
+    // status turn = X;
+    // int round = 1;
+
+    // while (victory == EMPTY)
+    // {
+    //     cout << "Round " << round << '\n';
+    //     sleep(2);
+
+    //     if (turn == X)
+    //     {
+    //         play(t, X, player1);
+    //         turn = O; // Change turn
+    //     }
+    //     else
+    //     {
+    //         play(t, O, player2);
+    //         turn = X; // Change turn
+    //     }
+
+    //     round++;
+    //     clear();
+    // }
     
-    /* Game result */
-    t.print_tic_tac_toe();
-    switch (victory)
-    {
-        case X:
-            cout << player1 << " Win !!!\n";
-            break;
-        case O:
-            cout << player2 << " Win !!!\n";
-            break;
-        case TIE:
-            cout << "It's a Tie\n";
-            break;
-        default:
-            break;
-    }
+    // /* Game result */
+    // t.print_tic_tac_toe();
+    // switch (victory)
+    // {
+    //     case X:
+    //         cout << player1 << " Win !!!\n";
+    //         break;
+    //     case O:
+    //         cout << player2 << " Win !!!\n";
+    //         break;
+    //     case TIE:
+    //         cout << "It's a Tie\n";
+    //         break;
+    //     default:
+    //         break;
+    // }
 
     return 0;
 }
