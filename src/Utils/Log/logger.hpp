6757b027a92e669eb0db6b10f3eb6c02896b657c
MAACJR032 <maacamposjr@gmail.com>
2024-10-31 20:50:30 -0300

    Enhanced Logf safety
    Logf checks for empty arguments

--- src/Utils/Log/logger.hpp
+++ src/Utils/Log/logger.hpp
@@ -12,26 +12,33 @@ class logger
         template <typename ...Args>
         void Logf(const std::string &prefix, const std::string &format, Args&&... args)
         {
-            // Allocate a buffer large enough to hold the formatted string
-            int size = std::snprintf(nullptr, 0, format.c_str(), args...) + 1; // Add 1 for the null terminator
-            if (size <= 0)
-            { 
-                std::cerr << "Error during formatting\n";
-                return;
+            if constexpr (sizeof...(args) == 0)
+            {
+                Error("Logf requires at least one formatting argument");
             }
+            else
+            {
+                // Allocate a buffer large enough to hold the formatted string
+                int size = std::snprintf(nullptr, 0, format.c_str(), args...) + 1; // Add 1 for the null terminator
+                if (size <= 0)
+                { 
+                    std::cerr << "Error during formatting\n";
+                    return;
+                }
 
-            std::string buffer(size, '\0');
-            std::snprintf(&buffer[0], size, format.c_str(), std::forward<Args>(args)...);
+                std::string buffer(size, '\0');
+                std::snprintf(&buffer[0], size, format.c_str(), std::forward<Args>(args)...);
 
-            // Get the current system time
-            auto now = std::chrono::system_clock::now();
-            std::time_t curr_time = std::chrono::system_clock::to_time_t(now);
-            
-            // Convert to a string and remove the newline
-            std::string time_str = std::ctime(&curr_time);
-            time_str.pop_back();
+                // Get the current system time
+                auto now = std::chrono::system_clock::now();
+                std::time_t curr_time = std::chrono::system_clock::to_time_t(now);
+                
+                // Convert to a string and remove the newline
+                std::string time_str = std::ctime(&curr_time);
+                time_str.pop_back();
 
-            std::cout << prefix << time_str << " " << buffer << '\n';
+                std::cout << prefix << time_str << " " << buffer << '\n';
+            }
         }
         
     public:
